cc1467402616dfca23e3d37e92a59542450b21f7
Alexander Nedovizin <parkit74@gmail.com>
2020-05-28 00:32:47 +0500

    smooth: user interface

---
+++ simple_user_interface.py
@@ -0,0 +1,71 @@
+import pygame
+import simple_draw as sd
+
+
+class Button:
+    def __init__(self, x, y, caption, event=None):
+        self.x = x
+        self.y = y
+        self.caption = caption
+        self._event = event
+        self._color1 = sd.COLOR_YELLOW
+        self._color2 = sd.COLOR_DARK_YELLOW
+        self._height = 10
+        self._width = 30
+
+    def set_size(self, width, height):
+        self._height = height
+        self._width = width
+        return self
+
+    def set_color_passive(self, color):
+        self._color1 = color
+        return self
+
+    def set_color_active(self, color):
+        self._color2 = color
+        return self
+
+    def draw(self, is_passive=True):
+        color1 = self._color1 if is_passive else self._color2
+        color2 = self._color2 if is_passive else self._color1
+
+        sd.rectangle(left_bottom=sd.get_point(self.x, self.y),
+                     right_top=sd.get_point(self.x + self._width, self.y + self._height),
+                     color=color1,
+                     width=0)
+        sd.rectangle(left_bottom=sd.get_point(self.x, self.y),
+                     right_top=sd.get_point(self.x + self._width, self.y + self._height),
+                     color=color2,
+                     width=2)
+        self._draw_text_on_button()
+
+    def _draw_text_on_button(self):
+        myfont = pygame.font.SysFont('Comic Sans MS', int(self._height * 0.7))
+        textsurface = myfont.render(self.caption, False, (0, 0, 0))
+        sd._screen.blit(textsurface,
+                        (self.x + (self._width - textsurface.get_width()) // 2,
+                         sd.resolution[1] - self.y + (self._height - textsurface.get_height()) // 2 - self._height))
+
+    def check_over(self, cursor_pos):
+        return (not cursor_pos is None
+                and self.x <= cursor_pos.x <= self.x + self._width
+                and self.y <= cursor_pos.y <= self.y + self._height)
+
+
+class UserInterface:
+
+    def __init__(self):
+        self.buttons = []
+
+    def add_button(self, x, y, caption, event=None):
+        new_button = Button(x=x, y=y, caption=caption, event=event)
+        self.buttons.append(new_button)
+        return new_button
+
+    def show(self, cursor_pos, is_click=False):
+        for button in self.buttons:
+            is_over = button.check_over(cursor_pos)
+            button.draw(is_over)
+            if is_over and is_click:
+                button._event()
--- smooth.py
+++ smooth.py
@@ -1,5 +1,6 @@
 import simple_draw as sd
-import pygame
+
+from simple_user_interface import UserInterface
 from splines import loopResolve
 
 EMPTY_MODE = 0
@@ -10,76 +11,6 @@ LEFT_CLICK = 3
 mode = EMPTY_MODE
 
 sd.caption = "Интерполяция"
-
-
-class Button:
-    def __init__(self, x, y, caption, event=None):
-        self.x = x
-        self.y = y
-        self.caption = caption
-        self._event = event
-        self._color1 = sd.COLOR_YELLOW
-        self._color2 = sd.COLOR_DARK_YELLOW
-        self._height = 10
-        self._width = 30
-
-    def set_size(self, width, height):
-        self._height = height
-        self._width = width
-        return self
-
-    def set_color_passive(self, color):
-        self._color1 = color
-        return self
-
-    def set_color_active(self, color):
-        self._color2 = color
-        return self
-
-    def draw(self, is_passive=True):
-        color1 = self._color1 if is_passive else self._color2
-        color2 = self._color2 if is_passive else self._color1
-
-        sd.rectangle(left_bottom=sd.get_point(self.x, self.y),
-                     right_top=sd.get_point(self.x + self._width, self.y + self._height),
-                     color=color1,
-                     width=0)
-        sd.rectangle(left_bottom=sd.get_point(self.x, self.y),
-                     right_top=sd.get_point(self.x + self._width, self.y + self._height),
-                     color=color2,
-                     width=2)
-        self._draw_text_on_button()
-
-    def _draw_text_on_button(self):
-        # pygame.font.init()
-        myfont = pygame.font.SysFont('Comic Sans MS', int(self._height * 0.8))
-        textsurface = myfont.render(self.caption, False, (0, 0, 0))
-        sd._screen.blit(textsurface,
-                        (self.x + (self._width - textsurface.get_width()) // 2,
-                         sd.resolution[1] - self.y + (self._height - textsurface.get_height()) // 2 - self._height))
-
-    def check_over(self, cursor_pos):
-        return (not cursor_pos is None
-                and self.x <= cursor_pos.x <= self.x + self._width
-                and self.y <= cursor_pos.y <= self.y + self._height)
-
-
-class UserInterface:
-
-    def __init__(self):
-        self.buttons = []
-
-    def add_button(self, x, y, caption, event=None):
-        new_button = Button(x=x, y=y, caption=caption, event=event)
-        self.buttons.append(new_button)
-        return new_button
-
-    def show(self, cursor_pos, is_click=False):
-        for button in self.buttons:
-            is_over = button.check_over(cursor_pos)
-            button.draw(is_over)
-            if is_over and is_click:
-                button._event()
 
 
 class Figure:
@@ -110,40 +41,43 @@ def clear_screen():
     sd.rectangle(left_bottom=sd.get_point(0, 0), right_top=sd.get_point(*sd.resolution), color=sd.background_color)
 
 
+def set_event(func):
+    def wrapper(figure):
+        def surrogate():
+            func(figure)
+        return surrogate
+    return wrapper
+
+
+@set_event
 def test_main_spline(figure):
-    def wrap_test_main_spline():
-        loopResolve(verts=figure.points, step=0, dist=figure.figure_resolution)
-    return wrap_test_main_spline
+    loopResolve(verts=figure.points, step=0, dist=figure.figure_resolution)
 
 
+@set_event
 def clear(figure):
-    def wrap_clear():
-        figure.points = []
-    return wrap_clear
+    figure.points = []
 
 
+@set_event
 def resolution_minus(figure):
-    def wrap_resolution_minus():
-        figure.figure_resolution += 10
-        loopResolve(verts=figure.points, step=0, dist=figure.figure_resolution)
-    return wrap_resolution_minus
+    figure.figure_resolution += 10
+    loopResolve(verts=figure.points, step=0, dist=figure.figure_resolution)
 
 
+@set_event
 def resolution_plus(figure):
-    def wrap_resolution_plus():
-        figure.figure_resolution -= 10
-        figure.figure_resolution = max(10, figure.figure_resolution)
-        loopResolve(verts=figure.points, step=0, dist=figure.figure_resolution)
-    return wrap_resolution_plus
+    figure.figure_resolution -= 10
+    figure.figure_resolution = max(10, figure.figure_resolution)
+    loopResolve(verts=figure.points, step=0, dist=figure.figure_resolution)
 
 
+@set_event
 def flip_radius(figure):
-    def wrap_flip_radius():
-        if figure.radius == 0:
-            figure.radius = 4
-        else:
-            figure.radius = 0
-    return wrap_flip_radius
+    if figure.radius == 0:
+        figure.radius = 4
+    else:
+        figure.radius = 0
 
 
 if __name__ == "__main__":
