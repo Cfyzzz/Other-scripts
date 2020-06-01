77cea61bb7db6c9ad487abf4dd341287eb44c614
Alexander Nedovizin <parkit74@gmail.com>
2020-06-01 23:14:44 +0500

    showreal: кнопка старта

--- showreal.py
+++ showreal.py
@@ -17,17 +17,15 @@ def clear_screen():
     sd.rectangle(left_bottom=sd.get_point(0, 0), right_top=sd.get_point(*sd.resolution), color=sd.background_color)
 
 
-def set_event(func):
-    def wrapper(figure):
-        def surrogate():
-            func(figure)
-        return surrogate
-    return wrapper
+def strat_showreal():
+    pass
 
 
 if __name__ == "__main__":
     sd._init()
-
+    sd.background_color = sd.COLOR_BLACK
+    user_interface = UserInterface()
+    user_interface.add_button(10, 10, "run", event=strat_showreal).set_size(60, 30)
     while True:
         mouse_pos, mouse_buttons = sd.get_mouse_state()
         point = mouse_pos
@@ -48,7 +46,7 @@ if __name__ == "__main__":
 
         sd.start_drawing()
         clear_screen()
-
+        user_interface.show(cursor_pos=mouse_pos, is_click=on_click)
         sd.finish_drawing()
         if sd.user_want_exit():
             break
